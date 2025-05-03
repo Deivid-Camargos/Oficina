14bd7a79b74f3c3d71c43a80b46415f4199a9cf7
Deivid Camargos <deivid.camargos@ufvjm.edu.br>
2025-05-03 15:36:46 -0300

    Adicionei o projeto

---
+++ src/main/java/com/deividcamargos/sistemaoficina/Controller/ControleAcesso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.deividcamargos.sistemaoficina.Controller;
+
+/**
+ *
+ * @author deivi
+ */
+public abstract class ControleAcesso {
+    
+}
